d1da51809a2e5b34e2fc04b73959ab5d4fbec158
Hendrock <107454519+Hendrock@users.noreply.github.com>
2025-06-22 20:42:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import torch
 import matplotlib.pyplot as plt
-from generator import CVAEDecoder
+from generator import CVAE
 
 @st.cache_resource
 def load_decoder():
-    model = CVAEDecoder()
+    model = CVAE()
     model.load_state_dict(torch.load("best_cvae.pth", map_location="cpu"))
     model.eval()
     return model
@@ -18,7 +18,7 @@ if st.button("Generate 5 Samples"):
     z = torch.randn(5, 20)
     labels = torch.full((5,), digit, dtype=torch.long)
     with torch.no_grad():
-        imgs = decoder(z, labels)
+        imgs = decoder.decode(z, labels)
     fig, axes = plt.subplots(1, 5, figsize=(10, 2))
     for ax, img in zip(axes, imgs):
         ax.imshow(img.squeeze(), cmap="gray")
