3f100b6b855dc647a4c3a9f1148dc1a4212a6f8e
Hendrock <107454519+Hendrock@users.noreply.github.com>
2025-06-22 20:42:59 +0700

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,17 +5,18 @@ class CVAE(nn.Module):
     def __init__(self, latent_dim=20, n_classes=10, img_shape=(1, 28, 28)):
         super().__init__()
         self.label_emb = nn.Embedding(n_classes, 10)
+=
         self.decoder = nn.Sequential(
             nn.Linear(latent_dim + 10, 400),
             nn.ReLU(),
             nn.Linear(400, 28 * 28),
             nn.Sigmoid()
         )
+
         self.img_shape = img_shape
 
     def decode(self, z, labels):
         label = self.label_emb(labels)
         z = torch.cat([z, label], dim=1)
         out = self.decoder(z)
-        out = out.view(-1, *self.img_shape)
-        return out
+        return out.view(-1, *self.img_shape)
